3b2fb5d2759f409b0b52513d889f132aa61cbffe
Robinson Amianda <robinsonamianda5@gmail.com>
2023-09-23 09:19:39 -0700

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution here!
+const cats=["Milo","Otis","Garfield"]
+destructivelyAppendCat = () => {
+    cats.push (`Ralph`)
+}
+const destructivelyPrependCat = () => {
+    cats.unshift (`Bob`)
+}
+const destructivelyRemoveLastCat = () => {
+    cats.pop ()
+}
+const destructivelyRemoveFirstCat = () => {
+    cats.shift ()
+}
+const appendCat = () => {
+    return [...cats,`Broom`]
+}
+const prependCat = () => {
+    return [`Arnold`,...cats]
+}
+let removeLastCat = () => {
+    return cats.slice(0,2)
+}
+removeFirstCat = () =>{
+    return cats.slice(1)
+}
